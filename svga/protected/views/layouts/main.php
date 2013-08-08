ab17078cdbe0094111de5b0bc869190e15c2961a
xexu <zherlonlogistics@gmail.com>
2013-08-08 12:30:19 +0200

    initial commit

---
+++ svga/protected/views/layouts/main.php
@@ -0,0 +1,71 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ca_ES" lang="ca_ES">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="ca_ES" />
+
+
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl; ?>/css/styles.css" />
+
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+
+	<?php Yii::app()->bootstrap->register(); ?>
+</head>
+
+<body>
+<?php $this->widget('bootstrap.widgets.TbNavbar',array(
+    
+    'items'=>array(
+    	
+        array(
+            'class'=>'bootstrap.widgets.TbMenu',
+            'items'=>array(
+                array('label'=>'Home', 'url'=> $this->createUrl('site/index')),
+                array('label'=>'Contact', 'url'=> $this->createUrl('site/contact')),
+                array('label'=>'Login', 'url'=> $this->createUrl('user/login'), 'visible'=> Yii::app()->user->isGuest),
+                array('label'=>'Logout ('. Yii::app()->user->getState('name') .')', 'url'=>$this->createUrl('user/logout'), 'visible'=>!Yii::app()->user->isGuest),
+            ),
+        ),
+        // 'brand'=>CHtml::image(Yii::app()->getBaseUrl().'/images/koala.jpg'),
+        '<form class="navbar-search pull-right">
+                <input type="text" class="search-query span3" placeholder="Search">
+                <div class="icon-search"></div>
+            </form>'
+    ),
+
+)); ?>
+
+<div class="container" id="page">
+	
+	<?php if(isset($this->breadcrumbs)):?>
+		<?php $this->widget('bootstrap.widgets.TbBreadcrumbs', array(
+			'links'=>$this->breadcrumbs,
+			'homeLink' => Chtml::link(Yii::t('SVGA', 'Inici'), $this->createUrl('site/index'))
+		)); ?><!-- breadcrumbs -->
+	<?php endif?>
+
+	<?php $this->widget('bootstrap.widgets.TbAlert', array(
+        'block'=>true, 
+        'fade'=>true, 
+        'closeText'=>'&times;', 
+	)); ?>
+
+	<?php echo $content; ?>
+
+	<div class="clear"></div>
+
+	<div id="footer">
+		Copyright &copy; <?php echo date('Y'); ?> by SVGA. || All Rights Reserved.<br/>
+		<span class="pull-right">
+			<?= CHtml::link(Yii::t('SVGA', 'Home'), $this->createUrl('site/index'))?> |
+			<?= CHtml::link(Yii::t('SVGA', 'Contacte'), $this->createUrl('site/contact'))?> |
+			<?= CHtml::link(Yii::t('SVGA', 'Política de privacitat'), $this->createUrl('site/privacy'))?> |
+			<?= CHtml::link(Yii::t('dSVGA', 'Quant a aquest web'), $this->createUrl('site/about'))?>
+		</span>
+	</div><!-- footer -->
+
+</div><!-- page -->
+
+</body>
+</html>
---
+++ svga/protected/views/site/index.php
@@ -0,0 +1,18 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+$this->breadcrumbs=array(
+	'Home',
+);
+?>
+
+<h1>Benvingut a <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+<p>Congratulations! You have successfully created your Yii application.</p>
+
+<p>You may change the content of this page by modifying the following two files:</p>
+<ul>
+	<li>View file: <code><?php echo __FILE__; ?></code></li>
+	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
+</ul>
